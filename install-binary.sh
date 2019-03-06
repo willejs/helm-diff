de2418241210f595100aa4f25fb9a433bea1cd03
Will Salt <williamejsalt@gmail.com>
2019-03-06 01:52:14 +0000

    modify install script for now

--- install-binary.sh
+++ install-binary.sh
@@ -3,7 +3,7 @@
 # Shamelessly copied from https://github.com/technosophos/helm-template
 
 PROJECT_NAME="helm-diff"
-PROJECT_GH="databus23/$PROJECT_NAME"
+PROJECT_GH="willejs/$PROJECT_NAME"
 
 : ${HELM_PLUGIN_PATH:="$(helm home --debug=false)/plugins/helm-diff"}
 
